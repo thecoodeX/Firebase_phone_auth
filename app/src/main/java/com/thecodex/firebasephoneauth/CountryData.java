442d9c6693189dc722d7aa71abc95076849e9c4b
Ankit Patel <ankitpatel@Ankits-MacBook-Air.local>
2019-03-29 20:05:16 +0530

    Initial commit

---
+++ app/src/main/java/com/thecodex/firebasephoneauth/CountryData.java
@@ -0,0 +1,7 @@
+package com.thecodex.firebasephoneauth;
+
+public class CountryData {
+    public static final String[] countryNames = {"India"};
+
+    public static final String[] countryAreaCodes = {"91"};
+}
